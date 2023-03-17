1355890a3e46297caa2e474681166aa6c561921b
Nik <nikstarall@gmail.com>
2023-03-18 06:54:58 +0700

    add con

--- web_gui.py
+++ web_gui.py
@@ -1,40 +1,16 @@
-import os
-from flask import Flask, flash, request, redirect, url_for, send_from_directory
-
-
-UPLOAD_FOLDER = 'uploads'
-
-
+from flask import Flask
+from flask import request, jsonify
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
+@app.route('/', methods=['GET'])
+def hello():
+    return jsonify({'version': 1})
 
-@app.route('/uploads/<name>')
-def download_file(name):
-    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
+def main():
+    app.run(host='0.0.0.0', port=8080)
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('Cant reed the file')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('Cant reed the file')
-            return redirect(request.url)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-        return redirect(url_for('download_file', name=file.filename))
-    return '''
-    <!doctype html>
-    <title>Загрузить новый файл</title>
-    <h1>Загрузить новый файл</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    </html>
-    '''
+if __name__ == '__main__':
+    main()
